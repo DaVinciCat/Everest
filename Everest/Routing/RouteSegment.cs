9072e7206e1539d1a1f4883f7456494ef3bb81ec
a b <trlolly@gmail.com>
2022-04-10 23:55:53 +0300

    Отрефакторил, добавил стратегию для сегментов. Перенес логику из матчера в сегменты.

--- Everest/Routing/RouteSegment.cs
+++ Everest/Routing/RouteSegment.cs
@@ -1,52 +1,109 @@
-﻿namespace Everest.Routing
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using Everest.Utils;
+
+namespace Everest.Routing
 {
 	public abstract class RouteSegment
 	{
-		public string Type { get;}
-
 		public string Name { get; }
 
 		public RouteSegment Next { get; }
 
 		public bool HasNext => Next != null;
 
-		protected RouteSegment(string type, string name, RouteSegment next)
+		protected RouteSegment(string name, RouteSegment next)
 		{
-			Type = type;
 			Name = name;
 			Next = next;
 		}
+
+		public abstract bool Match(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters);
 	}
 
 	public class StringRouteSegment : RouteSegment
 	{
-		public new static string Type => "STRING";
-
 		public StringRouteSegment(string name, RouteSegment next) 
-			: base(Type, name, next)
+			: base(name, next)
 		{
 
+		}
+		
+		public override bool Match(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters) 
+		{
+			if (!iterator.MoveNext())
+			{
+				return false;
+			}
+
+			var current = iterator.Current;
+			var result = segment.Name == current;
+
+			if (result && segment.HasNext)
+			{
+				return segment.Next.Match(iterator, segment.Next, parameters);
+			}
+
+			return result;
 		}
 	}
 
 	public class ParamRouteSegment : RouteSegment
 	{
-		public new static string Type => "PARAM";
-
 		public ParamRouteSegment(string name, RouteSegment next) 
-			: base(Type, name, next)
+			: base(name, next)
 		{
+		}
+
+		public override bool Match(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters)
+		{
+			if (!iterator.MoveNext())
+			{
+				return false;
+			}
+
+			parameters.Add(segment.Name, iterator.Current);
+
+			if (segment.HasNext)
+			{
+				return segment.Next.Match(iterator, segment.Next, parameters);
+			}
+
+			return true;
 		}
 	}
 
 	public class SplatRouteSegment : RouteSegment
 	{
-		public new static string Type => "SPLAT";
+		public static char Separator { get; set; } = '/';
 
 		public SplatRouteSegment(string name, RouteSegment next)
-			: base(Type, name, next)
+			: base(name, next)
 		{
 
+		}
+
+		public override bool Match(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters)
+		{
+			if (!segment.HasNext)
+			{
+				while (iterator.MoveNext()) { }
+				return true;
+			}
+			
+			var values = new List<string>();
+			var result = true;
+			while (iterator.HasNext())
+			{
+				result = segment.Next.Match(iterator, segment.Next, parameters);
+				if (result)
+					break;
+
+				values.Add(iterator.Current);
+			}
+
+			parameters.Add(segment.Name, string.Join(Separator, values));
+			return result;
 		}
 	}
 }
--- Everest/Routing/RouteSegmentMatcher.cs
+++ Everest/Routing/RouteSegmentMatcher.cs
@@ -1,95 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using Everest.Utils;
 
 namespace Everest.Routing
 {
-	public delegate bool RouteSegmentMatchAction(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters);
-
 	public class RouteSegmentMatcher
 	{
-		public static char Separator { get; set; } = '/';
-
-		public Dictionary<string, RouteSegmentMatchAction> Matchers { get; }
-		
-		public RouteSegmentMatcher()
-		{
-			Matchers = new Dictionary<string, RouteSegmentMatchAction>
-			{
-				{ StringRouteSegment.Type, MatchStringRouteSegment },
-				{ ParamRouteSegment.Type, MatchParamRouteSegment },
-				{ SplatRouteSegment.Type, MatchSplatRouteSegment }
-			};
-		}
-
-		private bool MatchStringRouteSegment(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters)
-		{
-			if (!iterator.MoveNext())
-			{
-				return false;
-			}
-
-			var current = iterator.Current;
-			var result = segment.Name == current;
-
-			if (result && segment.HasNext)
-			{
-				if (!Matchers.TryGetValue(segment.Next.Type, out var action))
-					throw new ArgumentException($"Route segment type is not supported: {segment.Next.Type}.");
-
-				return action(iterator, segment.Next, parameters);
-			}
-
-			return result;
-		}
-
-		private bool MatchParamRouteSegment(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters)
-		{
-			if (!iterator.MoveNext())
-			{
-				return false;
-			}
-
-			parameters.Add(segment.Name, iterator.Current);
-
-			if (segment.HasNext)
-			{
-				if (!Matchers.TryGetValue(segment.Next.Type, out var action))
-					throw new ArgumentException($"Route segment type is not supported: {segment.Next.Type}.");
-
-				return action(iterator, segment.Next, parameters);
-			}
-
-			return true;
-		}
-
-		private bool MatchSplatRouteSegment(Iterator<string> iterator, RouteSegment segment, NameValueCollection parameters)
-		{
-			if (!segment.HasNext)
-			{
-				while (iterator.MoveNext()) { }
-				return true;
-			}
-			
-			if (!Matchers.TryGetValue(segment.Next.Type, out var action))
-				throw new ArgumentException($"Route segment type is not supported: {segment.Next.Type}.");
-
-			var values = new List<string>();
-			var result = true;
-			while (iterator.HasNext())
-			{
-				result = action(iterator, segment.Next, parameters);
-				if(result)
-					break;
-
-				values.Add(iterator.Current);
-			}
-
-			parameters.Add(segment.Name, string.Join(Separator, values));
-			return result;
-		}
-
 		public bool Match(RouteSegment segment, string url, NameValueCollection parameters = null)
 		{
 			parameters ??= new NameValueCollection();
@@ -97,10 +12,7 @@ namespace Everest.Routing
 			var split = url.SplitUrl();
 			var iterator = new Iterator<string>(split);
 
-			if (!Matchers.TryGetValue(segment.Type, out var action))
-				throw new ArgumentException($"Route segment type is not supported: {segment.Type}.");
-			
-			return action(iterator, segment, parameters) && !iterator.HasNext();
+			return segment.Match(iterator, segment, parameters) && !iterator.HasNext();
 		}
 
 	}
