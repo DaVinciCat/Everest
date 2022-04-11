de8e7fe78b00f7b7ca6aecba88f6cfb30e39a777
a b <trlolly@gmail.com>
2022-04-12 00:06:19 +0300

    Тесты подвезли!

---
+++ Everest.Tests/RoutingTests.cs
@@ -0,0 +1,64 @@
+using System.Collections.Specialized;
+using Everest.Routing;
+using Xunit;
+
+namespace Everest.Tests
+{
+	public class RoutingTests
+	{
+		[Fact]
+		public void True_FooBar_FooBar()
+		{
+			const string pattern = "/foo/bar";
+			const string url = "/foo/bar";
+
+			var builder = new RouteSegmentBuilder();
+			var parser = new RouteSegmentParser();
+			var segment = builder.Build(pattern);
+
+			Assert.True(parser.TryParse(segment, url, new NameValueCollection()));
+		}
+
+		[Fact]
+		public void False_FooBar_Bar()
+		{
+			const string pattern = "/foo/bar";
+			const string url = "/bar";
+
+			var builder = new RouteSegmentBuilder();
+			var parser = new RouteSegmentParser();
+			var segment = builder.Build(pattern);
+
+			Assert.False(parser.TryParse(segment, url, new NameValueCollection()));
+		}
+
+		[Fact]
+		public void False_FooBar_FooBaz()
+		{
+			const string pattern = "/foo/bar";
+			const string url = "/foo/baz";
+
+			var builder = new RouteSegmentBuilder();
+			var parser = new RouteSegmentParser();
+			var segment = builder.Build(pattern);
+
+			Assert.False(parser.TryParse(segment, url, new NameValueCollection()));
+		}
+
+		[Fact]
+		public void True_FooBarId_FooBar1_Pattern()
+		{
+			const string pattern = "/foo/bar/:id";
+			const string url = "/foo/bar/1";
+
+			var builder = new RouteSegmentBuilder();
+			var parser = new RouteSegmentParser();
+			var segment = builder.Build(pattern);
+			var parameters = new NameValueCollection();
+
+			Assert.True(parser.TryParse(segment, url, parameters));
+			Assert.True(parameters["id"] != null);
+			Assert.True(parameters["id"] == "1");
+		}
+	}
+}
