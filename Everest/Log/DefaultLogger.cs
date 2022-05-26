932aa908d910630ea573f09c00b211505c67ecc1
aliu.popov <aliu.popov@stc-spb.ru>
2022-05-26 14:34:38 +0300

    Добавил папку с логами

---
+++ Everest/Log/DefaultLogger.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Everest.Log
+{
+	public static class DefaultLogger
+	{
+		public static ILoggerFactory LoggerFactory { get; set; } = new NullLoggerFactory();
+
+		public static ILogger<T> CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+	}
+}
