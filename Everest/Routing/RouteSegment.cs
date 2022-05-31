93f952068a1edf060f3de2c5b50df0620698f7bc
aliu.popov <aliu.popov@stc-spb.ru>
2022-05-31 16:03:05 +0300

    Убрал расширение из отдельного файла

--- Everest/Routing/RouteSegment.cs
+++ Everest/Routing/RouteSegment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Text.RegularExpressions;
 using Everest.Utils;
@@ -269,6 +270,23 @@ namespace Everest.Routing
 			}
 
 			return true;
+		}
+	}
+
+	public static class RouteSegmentExtensions
+	{
+		public static string GetPath(this RouteSegment segment)
+		{
+			var segments = new List<string>();
+			GetPathImpl(segment);
+			return $"/{string.Join("/", segments)}";
+
+			void GetPathImpl(RouteSegment next)
+			{
+				segments.Add(next.Value);
+				if (next.HasNextSegment)
+					GetPathImpl(next.NextSegment);
+			}
 		}
 	}
 }
