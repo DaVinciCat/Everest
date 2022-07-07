b9b852a0aa8818f7aead3d43bdbfde3a71b703c5
aliu.popov <aliu.popov@stc-spb.ru>
2022-07-07 12:11:39 +0300

    Добавил коллекцию фич

---
+++ Everest/Utils/FeatureCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Everest.Utils
+{
+	public interface IFeatureCollection : IDictionary<Type, object>
+	{
+	}
+
+	public class FeatureCollection : Dictionary<Type, object>, IFeatureCollection
+	{
+
+	}
+
+	public static class FeatureCollectionExtensions
+	{
+		public static T Get<T>(this IFeatureCollection features) =>
+			features.TryGetValue(typeof(T), out var value) ? (T)value : default(T);
+
+		public static IFeatureCollection Set<T>(this IFeatureCollection features, T feature)
+		{
+			features[typeof(T)] = feature;
+			return features;
+		}
+	}
+}
