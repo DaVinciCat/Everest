40393ea1da833c985102c0500780957bcb7dbea3
a b <trlolly@gmail.com>
2023-07-30 22:57:51 +0300

    Изменил алгоритм отправки ответа

--- Everest/Http/HttpResponse.cs
+++ Everest/Http/HttpResponse.cs
@@ -1,89 +1,98 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Everest.Http
 {
-    public class HttpResponse
-    {
-        public bool ResponseSent { get; private set; }
+	public class HttpResponse
+	{
+		public bool ResponseSent { get; private set; }
 
 		/*https://learn.microsoft.com/ru-ru/dotnet/api/system.net.httplistenerresponse.contentlength64?view=net-7.0*/
 		public long ContentLength64
-        {
-	        get => response.ContentLength64;
-	        set => response.ContentLength64 = value;
-        }
-		
+		{
+			get => response.ContentLength64;
+			set => response.ContentLength64 = value;
+		}
+
 		public bool KeepAlive
-        {
-            get => response.KeepAlive;
-            set => response.KeepAlive = value;
-        }
+		{
+			get => response.KeepAlive;
+			set => response.KeepAlive = value;
+		}
 
-        public string ContentType
-        {
-            get => response.ContentType;
-            set => response.ContentType = value;
-        }
+		public string ContentType
+		{
+			get => response.ContentType;
+			set => response.ContentType = value;
+		}
 
-        public Encoding ContentEncoding
-        {
-            get => response.ContentEncoding ?? Encoding.UTF8;
-            set => response.ContentEncoding = value;
-        }
+		public Encoding ContentEncoding
+		{
+			get => response.ContentEncoding ?? Encoding.UTF8;
+			set => response.ContentEncoding = value;
+		}
 
-        public WebHeaderCollection Headers => response.Headers;
+		public WebHeaderCollection Headers => response.Headers;
 
-        public HttpStatusCode StatusCode
-        {
-            get => (HttpStatusCode)response.StatusCode;
-            set
-            {
-                response.StatusCode = (int)value;
-                response.StatusDescription = value.ToString();
-            }
-        }
-		
+		public HttpStatusCode StatusCode
+		{
+			get => (HttpStatusCode)response.StatusCode;
+			set
+			{
+				response.StatusCode = (int)value;
+				response.StatusDescription = value.ToString();
+			}
+		}
+
 		public bool HasBody => Body != null && Body.Length > 0;
 
-        public Stream Body { get; } = new MemoryStream();
+		public Stream Body { get; } = new MemoryStream();
 
-        public Stream OutputStream => response.OutputStream;
+		public Stream OutputStream => response.OutputStream;
 
-        private readonly HttpListenerResponse response;
+		private readonly HttpListenerResponse response;
 
-        public HttpResponse(HttpListenerResponse response)
-        {
-            this.response = response ?? throw new ArgumentNullException(nameof(response));
-            
-            StatusCode = HttpStatusCode.OK;
-            AppendHeader("Server", "Everest");
-        }
+		public HttpResponse(HttpListenerResponse response)
+		{
+			this.response = response ?? throw new ArgumentNullException(nameof(response));
 
-        public void AddHeader(string name, string value) => response.AddHeader(name, value);
+			StatusCode = HttpStatusCode.OK;
+			AppendHeader("Server", "Everest");
+		}
 
-        public void AppendHeader(string name, string value) => response.AppendHeader(name, value);
+		public void AddHeader(string name, string value) => response.AddHeader(name, value);
 
-        public void RemoveHeader(string name) => response.Headers.Remove(name);
-		
-        public async Task SendResponseAsync()
-        {
+		public void AppendHeader(string name, string value) => response.AppendHeader(name, value);
+
+		public void RemoveHeader(string name) => response.Headers.Remove(name);
+
+		public void Close() => response.Close();
+ 
+		public async Task SendResponseAsync()
+		{
 			try
 			{
-				if (Body != null)
+				if (HasBody)
 				{
 					Body.Position = 0;
-					using (var reader = new BinaryReader(Body, ContentEncoding))
+					ContentLength64 = Body.Length;
+
+					using (var br = new BinaryReader(Body, ContentEncoding))
 					{
-						var bytes = reader.ReadBytes((int)Body.Length);
-						response.ContentLength64 = bytes.Length;
-						await response.OutputStream.WriteAsync(bytes, 0, bytes.Length);
-						await response.OutputStream.FlushAsync();
-						response.OutputStream.Close();
+						var buffer = new byte[4 * 1024];
+						int read;
+						while ((read = br.Read(buffer, 0, buffer.Length)) > 0)
+						{
+							await OutputStream.WriteAsync(buffer, 0, read);
+							await OutputStream.FlushAsync();
+						}
+						
+						OutputStream.Close();
 					}
 				}
 			}
@@ -91,69 +100,69 @@ namespace Everest.Http
 			{
 				try
 				{
-					response.Close();
+					Close();
 				}
 				finally
 				{
 					ResponseSent = true;
 				}
 			}
-        }
-    }
+		}
+	}
 
-    public static class HttpResponseExtensions
+	public static class HttpResponseExtensions
 	{
 		public static async Task WriteAsync(this HttpResponse response, byte[] content)
 		{
-			if (response == null) 
+			if (response == null)
 				throw new ArgumentNullException(nameof(response));
-			
-			if (content == null) 
+
+			if (content == null)
 				throw new ArgumentNullException(nameof(content));
 
 
 			await response.Body.WriteAsync(content, 0, content.Length);
 		}
 
-        public static async Task WriteAsync(this HttpResponse response, string content)
+		public static async Task WriteAsync(this HttpResponse response, string content)
 		{
-			if (response == null) 
+			if (response == null)
 				throw new ArgumentNullException(nameof(response));
-			
-			if (content == null) 
+
+			if (content == null)
 				throw new ArgumentNullException(nameof(content));
 
 			var bytes = response.ContentEncoding.GetBytes(content);
 			await response.WriteAsync(bytes);
 		}
-        
-        public static async Task WriteTextAsync(HttpResponse response, string content)
+
+		public static async Task WriteTextAsync(HttpResponse response, string content)
 		{
 			if (response == null) throw
 				new ArgumentNullException(nameof(response));
 
 			if (content == null)
 				throw new ArgumentNullException(nameof(content));
-			
+
 			response.RemoveHeader("Content-Type");
 			response.AppendHeader("Content-Type", "text/plain; charset=utf-8");
 
 			await response.WriteAsync(content);
 		}
 
-        public static async Task WriteHtmlAsync(this HttpResponse response, string content)
-        {
-	        if (response == null) throw
-		        new ArgumentNullException(nameof(response));
+		public static async Task WriteHtmlAsync(this HttpResponse response, string content)
+		{
+			if (response == null) throw
+				new ArgumentNullException(nameof(response));
 
-	        if (content == null)
-		        throw new ArgumentNullException(nameof(content));
+			if (content == null)
+				throw new ArgumentNullException(nameof(content));
 
-	        response.RemoveHeader("Content-Type");
-	        response.AppendHeader("Content-Type", "text/html; charset=utf-8");
+			response.RemoveHeader("Content-Type");
+			response.AppendHeader("Content-Type", "text/html; charset=utf-8");
 
-	        await response.WriteAsync(content);
-        }
+			await response.WriteAsync(content);
+		}
 
 		public static async Task WriteJsonAsync<T>(this HttpResponse response, T content, JsonSerializerOptions options = null)
 		{
@@ -166,11 +175,11 @@ namespace Everest.Http
 			response.RemoveHeader("Content-Type");
 			response.AddHeader("Content-Type", "application/json");
 
-			var json = options == null ?  
-				JsonSerializer.Serialize(content) : 
+			var json = options == null ?
+				JsonSerializer.Serialize(content) :
 				JsonSerializer.Serialize(content, options);
 
 			await response.WriteAsync(json);
-        }
+		}
 	}
 }
