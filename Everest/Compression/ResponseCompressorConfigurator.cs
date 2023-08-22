8d3b79dddb4f0b7b76e7a27694fd84c304530ef4
DaVincisCat <mail@nomail.com>
2023-08-23 00:49:07 +0300

    Переименовал

--- Everest/Compression/ResponseCompressorConfigurator.cs
+++ Everest/Compression/ResponseCompressorConfigurator.cs
@@ -14,30 +14,30 @@ namespace Everest.Compression
 		{
 		}
 
-		public ResponseCompressorConfigurator AddCompression(string encoding, Func<Stream, Stream> compression)
+		public ResponseCompressorConfigurator AddCompressor(string encoding, Func<Stream, Stream> compressor)
 		{
-			ResponseCompressor.Compressors.Add(encoding, compression);
+			ResponseCompressor.Compressors.Add(encoding, compressor);
 			return this;
 		}
 	}
 	
 	public static class ResponseCompressorConfiguratorExtensions
 	{
-		public static ResponseCompressorConfigurator AddGzipCompression(this ResponseCompressorConfigurator configurator)
+		public static ResponseCompressorConfigurator AddGzipCompressor(this ResponseCompressorConfigurator configurator)
 		{
-			configurator.AddCompression("gzip", input => new GZipStream(input, CompressionLevel.Fastest));
+			configurator.AddCompressor("gzip", input => new GZipStream(input, CompressionLevel.Fastest));
 			return configurator;
 		}
 
-		public static ResponseCompressorConfigurator AddDeflateCompression(this ResponseCompressorConfigurator configurator)
+		public static ResponseCompressorConfigurator AddDeflateCompressor(this ResponseCompressorConfigurator configurator)
 		{
-			configurator.AddCompression("deflate", input => new DeflateStream(input, CompressionLevel.Fastest));
+			configurator.AddCompressor("deflate", input => new DeflateStream(input, CompressionLevel.Fastest));
 			return configurator;
 		}
 
-		public static ResponseCompressorConfigurator AddBrotliCompression(this ResponseCompressorConfigurator configurator)
+		public static ResponseCompressorConfigurator AddBrotliCompressor(this ResponseCompressorConfigurator configurator)
 		{
-			configurator.AddCompression("brotli", input => new BrotliStream(input, CompressionLevel.Fastest));
+			configurator.AddCompressor("brotli", input => new BrotliStream(input, CompressionLevel.Fastest));
 			return configurator;
 		}
 	}
