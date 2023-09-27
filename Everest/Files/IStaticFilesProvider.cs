3e24ee3a1135c3ee054e7ff7e9bda6014c236d21
aliu.popov <nomail@fakemail.ru>
2023-09-27 11:03:09 +0300

    Перенес исходники в src

--- Everest/Files/IStaticFilesProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Everest.Files
-{
-    public interface IStaticFilesProvider
-    {
-        bool TryGetFile(string filePath, out FileInfo file);
-    }
-}
--- Everest/Mime/IMimeTypesProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Everest.Mime
-{
-    public interface IMimeTypesProvider
-    {
-        bool TryGetMimeType(string fileExtension, out MimeType mimeType);
-    }
-}
