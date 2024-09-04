968f162242cb9a5f909459615d657748261571a7
DaVinciCat <cat@nomail.com>
2024-09-04 12:01:51 +0500

    Фикс расширений для логирования

---
+++ src/Everest/Utils/LoggerExtensions.cs
@@ -0,0 +1,134 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Everest.Utils
+{
+    public static class LoggerExtensions
+    {
+        public static void LogDebugIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Debug))
+            {
+                var @params = func(); 
+                logger.LogDebug(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogDebugIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Debug))
+            {
+                var @params = func();
+                logger.LogDebug(@params.Message, @params.Args);
+            }
+        }
+
+        public static void LogTraceIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Trace))
+            {
+                var @params = func(); 
+                logger.LogTrace(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogTraceIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Trace))
+            {
+                var @params = func();
+                logger.LogTrace(@params.Message, @params.Args);
+            }
+        }
+
+        public static void LogInformationIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Information))
+            {
+                var @params = func(); 
+                logger.LogInformation(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogInformationIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Information))
+            {
+                var @params = func();
+                logger.LogInformation(@params.Message, @params.Args);
+            }  
+        }
+
+        public static void LogWarningIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Warning))
+            {   
+                var @params = func(); 
+                logger.LogWarning(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogWarningIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Warning))
+            {
+                var @params = func();
+                logger.LogWarning(@params.Message, @params.Args);
+            }
+        }
+
+        public static void LogErrorIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Error))
+            {
+                var @params = func(); 
+                logger.LogError(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogErrorIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Error))
+            {
+                var @params = func();
+                logger.LogError(@params.Message, @params.Args);
+            }
+        }
+
+        public static void LogCriticalIfEnabled(this ILogger logger, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Critical))
+            {
+                var @params = func(); 
+                logger.LogCritical(@params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogCriticalIfEnabled(this ILogger logger, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(LogLevel.Critical))
+            {
+                var @params = func();
+                logger.LogCritical(@params.Message, @params.Args);
+            }
+        }
+
+        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, Func<(Exception Exception, string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(logLevel))
+            {
+                var @params = func(); 
+                logger.Log(logLevel, @params.Exception, @params.Message, @params.Args);
+            }
+        }
+
+        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, Func<(string Message, object[] Args)> func)
+        {
+            if(logger.IsEnabled(logLevel))
+            {
+                var @params = func();
+                logger.Log(logLevel, @params.Message, @params.Args);
+            }
+        }
+    }
+}
