92625e8b69ed4aed09408d630adf56c6a6d6c699
DaVinciCat <mail@nomail.com>
2024-10-27 02:41:18 +0300

    Добавил расширение для логгера

--- src/Everest/Swagger/ISwaggerUiGenerator.cs
+++ src/Everest/Swagger/ISwaggerUiGenerator.cs
@@ -1,7 +1,15 @@
-﻿namespace Everest.Swagger
+﻿using Everest.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace Everest.Swagger
 {
     public interface ISwaggerUiGenerator
     {
         void Generate();
+    }
+
+    public static partial class HasLoggerExtensions
+    {
+        public static ILogger GetLogger(this ISwaggerUiGenerator instance) => (instance as IHasLogger)?.Logger;
     }
 }
